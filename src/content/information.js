8dafc3dd711931f85a9eb641d4b6191e85a3bc2f
ziadul <mdzhhridoy@gmail.com>
2023-12-29 21:08:38 +0600

    update description section

--- src/content/information.js
+++ src/content/information.js
@@ -1,12 +1,12 @@
 //personal information start
 
 const userData = {
-  firstName: "<Randil",
-  lastName: "Tharusha/>",
+  firstName: "<Md. Ziadul ",
+  lastName: "Haque/>",
   title: "About Me",
-  img: "https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp",
+  img: "images/profile/profile.png",
   description:
-    "Hey there! 👋 I'm Randil, a passionate Software Engineering undergrad at Staffordshire University. 💻 By day, I'm a tech enthusiast exploring the wonders of coding, and by night, I transform into an aspiring full-stack developer. 🌌 My journey spans from crafting code to delving into the realms of IoT, cybersecurity, AI, and ML. When not in the digital world, you'll find me navigating the strategic landscape of League of Legends for some competitive fun. Let's connect and embark on an exciting adventure through the vast universe of technology! 🚀 ",
+    "Hey there! 👋 I'm Ziadul, a passionate Web Developer completed bachelor degree in CSE from North South University. 💻 I'm a tech enthusiast exploring the wonders of coding, I transform into an aspiring full-stack developer. 🌌 My journey spans from crafting code to delving into the realms of web development. When not in the digital world, you'll find me navigating the strategic landscape of League of Legends for some competitive fun. Let's connect and embark on an exciting adventure through the vast universe of technology! 🚀 ",
 };
 
 //personal information end
