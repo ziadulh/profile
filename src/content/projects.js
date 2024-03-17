40f610a3c6eaa3a16c240e3ef5cc05074491cf40
ziadul <mdzhhridoy@gmail.com>
2024-03-17 15:54:53 +0600

    url changed

---
+++ src/content/projects.js
@@ -0,0 +1,34 @@
+//projects start
+const projects = [
+  {
+    name: "HRMS",
+    description:
+      "Optimize workforce management with our HRMS software. Track attendance, manage leaves, salary processing, and maintain a centralized employee record database. Streamline HR tasks for enhanced efficiency and engagement.",
+    stack: ["PHP(Laravel)", "VueJs", "JQuery", "MySQL"],
+    sourceCode: "https://simplifiedhrm.com/",
+    img: "projects/hrms.jpg",
+    preview: "https://simplifiedhrm.com/",
+  },
+  {
+    name: "SMS",
+    description:
+      "Elevate school management with our integrated system. Effortlessly store student records, track attendance, and manage tuition payments in one user-friendly platform. Streamline administrative tasks for a more organized and efficient educational experience.",
+    stack: ["PHP(Laravel)", "Jquery", "MySQL"],
+    sourceCode: "https://dev-iems.bdskill.com/login",
+    img: "projects/sms.png",
+    // preview: "https://dev-iems.bdskill.com/login",
+    preview: "https://msc.bdskill.com/",
+  },
+  {
+    name: "Buyomo",
+    description:
+      "Explore a world of convenience and choice on our eCommerce site. Browse diverse products, enjoy user-friendly interfaces, and secure transactions. Discover quality goods, exclusive deals, and personalized shopping experiences. Elevate your online shopping with us.",
+    stack: ["PHP(Laravel)", "JQuery", "MySQL"],
+    sourceCode: "https://github.com/",
+    img: "projects/eCom.png",
+    preview: "#",
+  },
+];
+
+//projects end
+export default projects;
